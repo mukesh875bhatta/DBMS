194997e5533bb4d232e3d84cbfffd06c14405722
Mukesh Bhatta <135792134+mukesh875bhatta@users.noreply.github.com>
2024-07-11 08:52:56 +0545

    Add files via upload

---
+++ Gharbeti.sql
@@ -0,0 +1,8 @@
+CREATE TABLE gharbeti (
+    gharibeti_id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    address VARCHAR(200)
+);
+
---
+++ Location.sql
@@ -0,0 +1,7 @@
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    city VARCHAR(50),
+    area VARCHAR(100),
+    postal_code VARCHAR(10)
+);
+
